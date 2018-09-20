a6cdfbf0c391a2fb816e500356f84d9abc645ac7
Styku0 <styku94@gmail.com>
2018-09-20 13:25:29 +0200

    Zadania zrobione

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,28 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
-
 //Twój kod
+const crypto = require('crypto');
+
+const passwords = ['??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83'];
+
+function findPassword(hash, passwords, hashFuncton){
+    let result;
+    passwords.forEach(password => {
+    let newHash = crypto.createHmac(hashFuncton, password).digest('hex');
+    if(hash === newHash){
+        result = password;
+        return;
+    }
+    })
+    if(result){
+        return result;
+    }
+    else{
+        return "ERROR - Password not found.";
+    }
+    
+}
+
+console.log("The password is:", findPassword(MY_PWD_HASH, passwords, 'sha256'));
+console.log("The password is:", findPassword(MY_PWD_HASH, passwords, 'sha512'));
+console.log("The password is:", findPassword(MY_PWD_HASH, passwords, 'md5'));
+console.log("The password is:", findPassword(MY_PWD_HASH, passwords, 'rmd160'));
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
-//Twój kod
+// //Twój kod
+const fs = require("fs");
+const crypto = require("crypto");
+const path = process.argv[2];
+
+fs.readFile(path, "utf8", (err, data) => {
+
+    if(err === null){
+        console.log(data);
+        console.log(crypto.createHmac("sha256", data).digest("hex"));
+    }
+    else{
+        console.log("Błąd odczytu pliku.", err);
+    }
+})
